6141203daa9484714f5e87038cd9c24a70c24a03
ArMa$hka <krmnli.a@gmail.com>
2018-01-16 21:11:29 +0600

    onee

---
+++ begin/first/first/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            */
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+            Console.WriteLine(a + 123);
+
+            //Console.WriteLine(n1 + n2);
+            Console.ReadKey();
+        }
+    }
+}
