e12152edd7cb939f69a9d8fe9b6d2e56e84fcc17
ArMa$hka <krmnli.a@gmail.com>
2018-01-16 20:54:50 +0600

    commentttt

---
+++ ConsoleApp2/ConsoleApp2/circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class circle
+    {
+        public float radius;
+
+        public circle()
+        {
+            radius = 10;
+        }
+
+        public circle (float r)
+        {
+            radius r;
+        }
+    }
+}
