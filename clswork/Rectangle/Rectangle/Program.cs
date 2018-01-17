664fb979b95c0a1232e672e189f8baf07fa8bb5a
ArMa$hka <krmnli.a@gmail.com>
2018-01-17 10:13:31 +0600

    rect

---
+++ clswork/Rectangle/Rectangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+
+            Rect r = new Rect(l, w);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ clswork/Rectangle/Rectangle/Rect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Rect
+    {
+        public double length;
+        public double width;
+        public double area;
+
+        public Rect(double l, double w)
+        {
+            length = l;
+            width = w;
+            FindArea();
+        }
+
+        public void FindArea()
+        {
+            area = length * width;
+        }
+
+        public override string ToString()
+        {
+            return "length = " + length + "\nwidth = " + width + "\narea = " + area;
+        }
+    }
+
+    
+}
