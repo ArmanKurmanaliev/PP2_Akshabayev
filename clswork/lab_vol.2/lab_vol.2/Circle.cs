09f77a47ca9d5dff23dfe2a6e1a4b1908990160c
ArMa$hka <krmnli.a@gmail.com>
2018-01-17 09:52:04 +0600

    circ_clswrk

---
+++ clswork/lab_vol.2/lab_vol.2/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_vol._2
+{
+    class Circle
+    {
+        public double radius;
+        public double area;
+
+        public Circle()
+        {
+
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            FindArea();
+        }
+        public void FindArea()
+        {
+            area = Math.PI * radius * radius;
+        }
+
+        public override string ToString()
+        {
+            return "radius = " + radius + "\narea = " + area;
+        }
+
+
+    }
+
+    
+}
---
+++ clswork/lab_vol.2/lab_vol.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_vol._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            Circle c = new Circle(r);
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
