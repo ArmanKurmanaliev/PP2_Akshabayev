344bdf84a59a8aa9f858e50a3d4924fab39a861e
ArMa$hka <krmnli.a@gmail.com>
2018-01-24 10:12:50 +0600

    farrels

---
+++ clswork/far/far/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace far
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\");
+            
+            DirectoryInfo[] ds = directory.GetDirectories();
+            FileInfo[] fs = directory.GetFiles();
+
+            foreach (DirectoryInfo d in ds)
+                Console.WriteLine(d.Name);
+            foreach (FileInfo f in fs)
+                Console.WriteLine(f.Name);
+
+            Console.ReadKey();
+            
+
+            /*
+            FileSystemInfo[] fss = directory.GetFileSystemInfos();
+
+            foreach (FileSystemInfo a in fss)
+                Console.WriteLine(a.Name);
+
+            Console.ReadKey();
+            */
+        }
+    }
+}
---
+++ clswork/far2/far2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace far2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            while (true)
+            {
+                ConsoleKeyInfo k = Console.ReadKey();
+                Console.BackgroundColor = ConsoleColor.Black;
+                if (k.Key == ConsoleKey.UpArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Asdasdlasdkfjh");
+                }
+
+                if (k.Key == ConsoleKey.DownArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Asdasdlasdkfjh");
+                }
+
+                if (k.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Asdasdlasdkfjh");
+                }
+
+                if (k.Key == ConsoleKey.RightArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Asdasdlasdkfjh");
+                }
+
+                if (k.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                }
+
+                if (k.Key == ConsoleKey.Escape)
+                    break;
+            }
+
+            
+        }
+    }
+}
